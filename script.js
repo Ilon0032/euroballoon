60a785c81436385617608695713b49fd929e7825
Luna Baunkjær <lunabaunkjaer@Lunas-MacBook-Pro.local>
2019-12-18 11:24:10 +0100

    forside

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("siden vises");
     document.querySelector("#menuknap_mobil").addEventListener("click", klikMenu);
+
 }
 
 function klikMenu() {
     console.log("klik menu");
     document.querySelector(".nav_mobil").classList.remove("hide");
 }
+
+
+
+//
+//function mouseOut() {
+//    console.log("ikke over mus");
+//    document.querySelector(".tekst_forside").onmouseout.classList.add("hide");
+//}
+////
+//document.querySelector(".tekst_forside").onmouseover = function ({
+//    mouseOver
+//});
